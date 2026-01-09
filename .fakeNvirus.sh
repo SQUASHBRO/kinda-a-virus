19a48f7b64a1b9c3203ed191ed6daa40d15a43d8
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-09 15:12:40 -0800

    Add fakeNvirus.sh with warning message and curl command

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="be careful when downloading stuff"
+curl -s  https://counter-worker.brevikaugustus.workers.dev
