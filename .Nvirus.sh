a72fe655e9ef0ccc643370502aba2c0ecb78ff5c
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-09 14:03:51 -0800

    Update error message title in .Nvirus.sh

--- .Nvirus.sh
+++ .Nvirus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="Bro got hacked!"
+zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="be careful when downloading stuff"
