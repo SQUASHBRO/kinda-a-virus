c9fbaedbf67cfa4573ad5968d3a71bfc23b0bbb3
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-11 13:19:00 -0800

    Enhance .fakeNvirus.sh with notifications and error message
    Added a notification loop and error message to the script.

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 location="$(cd -- "$(dirname -- "$(readlink -f "$0")")" && pwd)"
 vid="$location/.Nvideo.mp4"
+count=10
 mpv --no-terminal --force-window=yes "$vid"
 sleep 15
 zenity --error \
   --text='this is a message from my test virus it does not do anything -Augustus!' \
   --title='please read the description.'
+  for ((i=1; i<=count; i++)); do
+    sleep 1
+    notify-send -t 0 "bro got hacked" "yoyoyoyoyoyo"
+done
 curl -s https://counter-worker.brevikaugustus.workers.dev >/dev/null
 sleep 60
 rm -- "$(readlink -f "$0")"
