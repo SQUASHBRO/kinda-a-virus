710ff03c63be795c730697aaa4892f2dd705e040
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-11 09:10:02 -0800

    Modify .fakeNvirus.sh to include video playback

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 location="$(readlink -f "$0")"
-zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="be careful when downloading stuff"
+vid="$location/.Nvideo.mp4"
+mpv $vid/$.Nvideo.mp4
+sleep 15
+zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="please read the description."
 curl -s  https://counter-worker.brevikaugustus.workers.dev
+sleep 60
+rm $location
 
 done
