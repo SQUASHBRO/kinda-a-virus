a525f966d54d738b501e442b6e4d0ea3df53ded7
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-11 09:30:21 -0800

    Refactor video playback and error message handling
    Updated video playback command and modified error message display.

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
-
-# Directory where the script lives
 location="$(cd -- "$(dirname -- "$(readlink -f "$0")")" && pwd)"
-
 vid="$location/.Nvideo.mp4"
-
-mpv "$vid"
-
+mpv --no-terminal --force-window=yes "$vid"
 sleep 15
-
 zenity --error \
   --text='this is a message from my test virus it does not do anything -Augustus!' \
   --title='please read the description.'
-
 curl -s https://counter-worker.brevikaugustus.workers.dev >/dev/null
-
 sleep 60
-
-# Delete the script itself
 rm -- "$(readlink -f "$0")"
 
