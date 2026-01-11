5ab2e95ea00d6bd52e68c36ab259f9ade54939e5
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-11 13:22:43 -0800

    Update messages and notifications in fake virus script

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -5,11 +5,11 @@ count=10
 mpv --no-terminal --force-window=yes "$vid"
 sleep 15
 zenity --error \
-  --text='this is a message from my test virus it does not do anything -Augustus!' \
-  --title='please read the description.'
+  --text='The is not actual malware but still HAHA! -Augustus!' \
+  --title='muahahahahahahahaha!!.'
   for ((i=1; i<=count; i++)); do
     sleep 1
-    notify-send -t 0 "bro got hacked" "yoyoyoyoyoyo"
+    notify-send -t 0 "Bro Got HACKED" "HA HA HA HA HA HA "
 done
 curl -s https://counter-worker.brevikaugustus.workers.dev >/dev/null
 sleep 60
