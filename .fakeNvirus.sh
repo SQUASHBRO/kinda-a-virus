d95416c3f293572f9bb5a6ced94d4503a9cf7bcb
SQUASHBRO <114193436+SQUASHBRO@users.noreply.github.com>
2026-01-11 08:49:58 -0800

    Add error message and curl command to script

--- .fakeNvirus.sh
+++ .fakeNvirus.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+location="$(readlink -f "$0")"
 zenity --error --text="this is a message from my test virus it doesn't do anything -Augustus\!" --title="be careful when downloading stuff"
 curl -s  https://counter-worker.brevikaugustus.workers.dev
+
 done
